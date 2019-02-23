f973aaecb861576e3099f8f7d9f98cb4952b1b9b
kexiii <17425627+Kexiii@users.noreply.github.com>
2019-02-23 22:04:56 +0800

    Advance packet version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pydouyu",
-    version="0.0.4",
+    version="0.0.5",
     author="kexiii",
     author_email="kexiii@163.com",
     description="Python implementation of douyu TV API",
