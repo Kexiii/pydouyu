494732159980b7b71575e6757899c48052c6c2e0
kexiii <17425627+Kexiii@users.noreply.github.com>
2021-01-28 14:12:46 +0800

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pydouyu",
-    version="0.0.5",
+    version="0.0.6",
     author="kexiii",
     author_email="kexiii@163.com",
     description="Python implementation of douyu TV API",
